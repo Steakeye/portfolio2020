3449907be8c7e92362cced95ff695213c726bd7b
Andrew Keats <andrew@steakeye.com>
2020-08-20 03:06:57 +0100

    wip: configuring scss and linting

--- rollup.config.js
+++ rollup.config.js
@@ -2,113 +2,174 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
+import scss from 'rollup-plugin-scss';
 import babel from '@rollup/plugin-babel';
+// import postcss from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from 'rollup-plugin-typescript2';
-import config from 'sapper/config/rollup.js';
+import config from 'sapper/config/rollup';
 import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
-const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+const onWarn = (warning, onWarnFn) => (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message))
+  || (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message))
+  || onWarnFn(warning);
+
+/* const scssConfiguration = (postfix) => ({
+	output: `public/assets/css/global-${postfix}.css`, */
+const scssConfiguration = () => ({
+  output: 'public/assets/css/global.css',
+  sourceMap: dev,
+  // prefix: '@import \'src/styles/variables.scss\';',
+  watch: 'src/styles/!*.scss',
+});
+
+const preProcessOptions = {
+  babel: {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          loose: true,
+          // No need for babel to resolve modules
+          modules: false,
+          targets: {
+            // ! Very important. Target es6+
+            esmodules: true,
+          },
+        },
+      ],
+    ],
+  },
+  defaults: {
+    script: 'typescript',
+    style: 'scss',
+  },
+  postcss: {
+    plugins: [require('autoprefixer')()],
+  },
+};
+
+const preProcessConfig = sveltePreprocess(preProcessOptions);
+
+const cssOutputFunc = (css) => {
+  css.write('public/assets/css/bundle.css', dev);
+};
 
 export default {
-	client: {
-		input: config.client.input().replace(/.js$/, '.ts'),
-		output: config.client.output(),
-		plugins: [
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true
-			,
-				preprocess: sveltePreprocess(),
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-		typescript({ sourceMap: dev }),
+  client: {
+    input: config.client.input().replace(/.js$/, '.ts'),
+    output: config.client.output(),
+    plugins: [
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      scss(scssConfiguration()),
+      svelte({
+        dev,
+        hydratable: true,
+        emitCss: true,
+        preprocess: preProcessConfig,
+        // we'll extract any component CSS out into
+        // a separate file — better for performance
+        css: cssOutputFunc,
+      }),
+      /* (postcss({
+				extract: 'assets/css/global.scss',
+				sourceMap: true,
+				minimize: true,
+			}), */
+      resolve({
+        browser: true,
+        dedupe: ['svelte'],
+      }),
+      commonjs(),
+      typescript({ sourceMap: dev }),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+      legacy
+        && babel({
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
+          babelHelpers: 'runtime',
+          exclude: ['node_modules/@babel/**'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 0.25%, not dead',
+              },
+            ],
+          ],
+          plugins: [
+            '@babel/plugin-syntax-dynamic-import',
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                useESModules: true,
+              },
+            ],
+          ],
+        }),
 
-			!dev && terser({
-				module: true
-			})
-		],
+      !dev
+        && terser({
+          module: true,
+        }),
+    ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	},
+    preserveEntrySignatures: false,
+    onwarn: onWarn,
+  },
 
-	server: {
-		input: config.server.input().server.replace(/.js$/, '.ts'),
-		output: config.server.output(),
-		plugins: [
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				generate: 'ssr',
-				hydratable: true,
-				dev
-			,
-				preprocess: sveltePreprocess(),
-			}),
-			resolve({
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-		typescript({ sourceMap: dev }),
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+  server: {
+    input: config.server.input().server.replace(/.js$/, '.ts'),
+    output: config.server.output(),
+    plugins: [
+      replace({
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      scss(scssConfiguration()),
+      svelte({
+        generate: 'ssr',
+        hydratable: true,
+        dev,
+        preprocess: preProcessConfig,
+        // we'll extract any component CSS out into
+        // a separate file — better for performance
+        css: cssOutputFunc,
+      }),
+      resolve({
+        dedupe: ['svelte'],
+      }),
+      commonjs(),
+      typescript({ sourceMap: dev }),
+    ],
+    external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 
-		preserveEntrySignatures: 'strict',
-		onwarn,
-	},
+    preserveEntrySignatures: 'strict',
+    onwarn: onWarn,
+  },
 
-	serviceworker: {
-		input: config.serviceworker.input().replace(/.js$/, '.ts'),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-		typescript({ sourceMap: dev }),
-			!dev && terser()
-		],
+  serviceworker: {
+    input: config.serviceworker.input().replace(/.js$/, '.ts'),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      commonjs(),
+      typescript({ sourceMap: dev }),
+      !dev && terser(),
+    ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	}
+    preserveEntrySignatures: false,
+    onwarn: onWarn,
+  },
 };
--- scripts/setupTypeScript.js
+++ scripts/setupTypeScript.js
@@ -1,107 +1,136 @@
-// @ts-check
-
 /** This script modifies the project to support TS code in .svelte files like:
 
   <script lang="ts">
   	export let name: string;
   </script>
- 
-  As well as validating the code for CI.
-  */
 
-/**  To work on this script:
-  rm -rf test-template template && git clone sveltejs/template test-template && node scripts/setupTypeScript.js test-template
+  As well as validating the code for CI.
 */
 
-const fs = require("fs");
-const path = require("path");
-const { argv } = require("process");
+/**  To work on this script:
+  rm -rf test-template template && git clone sveltejs/template test-template \
+  && node scripts/setupTypeScript.js test-template
+*/
 
-const projectRoot = argv[2] || path.join(__dirname, "..");
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('process');
+
+const projectRoot = argv[2] || path.join(__dirname, '..');
 
 // Add deps to pkg.json
-const packageJSON = JSON.parse(fs.readFileSync(path.join(projectRoot, "package.json"), "utf8"))
+const packageJSON = JSON.parse(fs.readFileSync(path.join(projectRoot, 'package.json'), 'utf8'));
 packageJSON.devDependencies = Object.assign(packageJSON.devDependencies, {
-  "svelte-check": "^1.0.0",
-  "svelte-preprocess": "^4.0.0",
-  "rollup-plugin-typescript2": "^0.27.2",
-  "typescript": "^3.9.3",
-  "tslib": "^2.0.0",
-  "@tsconfig/svelte": "^1.0.0"
+  'svelte-check': '^1.0.0',
+  'svelte-preprocess': '^4.0.0',
+  'rollup-plugin-typescript2': '^0.27.2',
+  typescript: '^3.9.3',
+  tslib: '^2.0.0',
+  '@tsconfig/svelte': '^1.0.0',
 });
 
 // Add script for checking
 packageJSON.scripts = Object.assign(packageJSON.scripts, {
-  "validate": "svelte-check"
+  validate: 'svelte-check',
 });
 
 // Write the package JSON
-fs.writeFileSync(path.join(projectRoot, "package.json"), JSON.stringify(packageJSON, null, "  "));
+fs.writeFileSync(path.join(projectRoot, 'package.json'), JSON.stringify(packageJSON, null, '  '))
 
 // mv src/*.js to *.ts - note, we need to edit rollup.config.js for this too
-['client', 'server', 'service-worker'].forEach((fileName) => {
-  const beforeJSPath = path.join(projectRoot, "src", `${fileName}.js`)
-  const afterTSPath = path.join(projectRoot, "src", `${fileName}.ts`)
-  fs.renameSync(beforeJSPath, afterTSPath)
+;['client', 'server', 'service-worker'].forEach((fileName) => {
+  const beforeJSPath = path.join(projectRoot, 'src', `${fileName}.js`);
+  const afterTSPath = path.join(projectRoot, 'src', `${fileName}.ts`);
+  fs.renameSync(beforeJSPath, afterTSPath);
 });
 
-//Update service worker ts with `self` declaration
-const serviceWorkerPath = path.join(projectRoot, "src", 'service-worker.ts');
-let serviceWorker = fs.readFileSync(serviceWorkerPath, "utf8");
-fs.writeFileSync(serviceWorkerPath, 'declare let self: ServiceWorkerGlobalScope\n' + serviceWorker);
+// Update service worker ts with `self` declaration
+const serviceWorkerPath = path.join(projectRoot, 'src', 'service-worker.ts');
+const serviceWorker = fs.readFileSync(serviceWorkerPath, 'utf8');
+fs.writeFileSync(serviceWorkerPath, `declare let self: ServiceWorkerGlobalScope\n${serviceWorker}`)
 
 // Switch the *.svelte file to use TS
-[{
-  view: 'index',
-},{
-  view: 'about',
-},{
-  view: '_layout',
-  vars: [{ name:  'segment', type: 'string' }]
-},{
-  view: '_error',
-  vars: [{ name:  'status', type: 'number' }, { name:  'error', type: 'Error' }]
-},{
-  view: 'blog/index',
-  vars: [{ name:  'posts', type: 'any[]' }]
-},{
-  view: 'blog/[slug]',
-  vars: [{ name:  'post', type: 'any' }]
-}].forEach(({ view, vars }) => {
-  const svelteFilePath = path.join(projectRoot, "src/routes", `${view}.svelte`)
-  let file = fs.readFileSync(svelteFilePath, "utf8")
+;[
+  {
+    view: 'components/Nav',
+    vars: [{ name: 'segment', type: 'string' }],
+  },
+  {
+    view: 'routes/index',
+  },
+  {
+    view: 'routes/about',
+  },
+  {
+    view: 'routes/_layout',
+    vars: [{ name: 'segment', type: 'string' }],
+  },
+  {
+    view: 'routes/_error',
+    vars: [
+      { name: 'status', type: 'number' },
+      { name: 'error', type: 'Error' },
+    ],
+  },
+  {
+    view: 'routes/blog/index',
+    vars: [{ name: 'posts', type: 'any[]' }],
+  },
+  {
+    view: 'routes/blog/[slug]',
+    vars: [{ name: 'post', type: 'any' }],
+  },
+].forEach(({ view, vars }) => {
+  const svelteFilePath = path.join(projectRoot, 'src', `${view}.svelte`);
+  let file = fs.readFileSync(svelteFilePath, 'utf8');
 
-  file = file.replace(/(?:<script)(( .*?)*?)>/gm, '<script$1 lang="ts">')
+  file = file.replace(/(?:<script)(( .*?)*?)>/gm, '<script$1 lang="ts">');
 
   if (vars) {
     vars.forEach(({ name, type }) => {
-      file = file.replace(`export let ${name};`, `export let ${name}: ${type};`)
+      file = file.replace(`export let ${name};`, `export let ${name}: ${type};`);
     });
   }
 
-  fs.writeFileSync(svelteFilePath, file)
+  fs.writeFileSync(svelteFilePath, file);
 });
 
 // Edit rollup config
-const rollupConfigPath = path.join(projectRoot, "rollup.config.js")
-let rollupConfig = fs.readFileSync(rollupConfigPath, "utf8")
+const rollupConfigPath = path.join(projectRoot, 'rollup.config.js');
+let rollupConfig = fs.readFileSync(rollupConfigPath, 'utf8');
 
 // Edit imports
-rollupConfig = rollupConfig.replace(`'rollup-plugin-terser';`, `'rollup-plugin-terser';
+rollupConfig = rollupConfig.replace(
+  '\'rollup-plugin-terser\';',
+  `'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import typescript from 'rollup-plugin-typescript2';`)
+import typescript from 'rollup-plugin-typescript2';`,
+);
 
 // Replace name of entry points, client and server
-rollupConfig = rollupConfig.replace(/input: config.(?:client|serviceworker).input\(\)/gm, `$&.replace(/\.js$/, '.ts')`)
-rollupConfig = rollupConfig.replace(/input: config.server.input\(\)/gm, `$&.server.replace(/\.js$/, '.ts')`)
+/* eslint-disable no-useless-escape */
+rollupConfig = rollupConfig.replace(
+  /input: config.(?:client|serviceworker).input\(\)/gm,
+  '$&.replace(/.js$/, \'.ts\')',
+);
+rollupConfig = rollupConfig.replace(
+  /input: config.server.input\(\)/gm,
+  '$&.server.replace(/.js$/, \'.ts\')',
+);
 
 // Add preprocess to the svelte config,
-rollupConfig = rollupConfig.replace(/(\n)([ \t]*)(svelte\({[\s\w\W]*?)(})\W*\)/gm, '$1$2$3,\n$2\tpreprocess: sveltePreprocess(),\n$2$4)')
+rollupConfig = rollupConfig.replace(
+  /(\n)([ \t]*)(svelte\({[\s\w\W]*?)(})\W*\)/gm,
+  '$1$2$3,\n$2\tpreprocess: sveltePreprocess(),\n$2$4)',
+);
 
 // Add TypeScript
-rollupConfig = rollupConfig.replace(/commonjs\(\),?/gm, 'commonjs(),\n\t\ttypescript({ sourceMap: dev }),')
+rollupConfig = rollupConfig.replace(
+  /commonjs\(\),?/gm,
+  'commonjs(),\n\t\ttypescript({ sourceMap: dev }),',
+);
 
-fs.writeFileSync(rollupConfigPath, rollupConfig)
+fs.writeFileSync(rollupConfigPath, rollupConfig);
 
 // Add TSConfig
 const tsconfig = `{
@@ -115,47 +144,53 @@ const tsconfig = `{
   },
   "include": ["src/**/*"],
   "exclude": ["node_modules/*", "__sapper__/*", "public/*"]
-}`
-const tsconfigPath =  path.join(projectRoot, "tsconfig.json")
-fs.writeFileSync(tsconfigPath, tsconfig)
+}`;
+const tsconfigPath = path.join(projectRoot, 'tsconfig.json');
+fs.writeFileSync(tsconfigPath, tsconfig);
 
 // Adds typings folder and sapper typings
-const typingsFolderPath = path.join(projectRoot, "typings");
-const sapperTypingsFolderPath = path.join(typingsFolderPath, "@sapper");
-fs.mkdirSync(typingsFolderPath)
-fs.mkdirSync(sapperTypingsFolderPath)
-fs.writeFileSync(path.join(sapperTypingsFolderPath, "index.d.ts"), `declare module '@sapper/app';
+const typingsFolderPath = path.join(projectRoot, 'typings');
+const sapperTypingsFolderPath = path.join(typingsFolderPath, '@sapper');
+fs.mkdirSync(typingsFolderPath);
+fs.mkdirSync(sapperTypingsFolderPath);
+fs.writeFileSync(
+  path.join(sapperTypingsFolderPath, 'index.d.ts'),
+  `declare module '@sapper/app';
 declare module '@sapper/server';
 declare module '@sapper/service-worker';
-`)
+`,
+);
 
 // Delete this script, but not during testing
 if (!argv[2]) {
   // Remove the script
-  fs.unlinkSync(path.join(__filename))
+  fs.unlinkSync(path.join(__filename));
 
   // Check for Mac's DS_store file, and if it's the only one left remove it
-  const remainingFiles = fs.readdirSync(path.join(__dirname))
+  const remainingFiles = fs.readdirSync(path.join(__dirname));
   if (remainingFiles.length === 1 && remainingFiles[0] === '.DS_store') {
-    fs.unlinkSync(path.join(__dirname, '.DS_store'))
+    fs.unlinkSync(path.join(__dirname, '.DS_store'));
   }
 
   // Check if the scripts folder is empty
   if (fs.readdirSync(path.join(__dirname)).length === 0) {
     // Remove the scripts folder
-    fs.rmdirSync(path.join(__dirname))
+    fs.rmdirSync(path.join(__dirname));
   }
 }
 
 // Adds the extension recommendation
-fs.mkdirSync(path.join(projectRoot, ".vscode"))
-fs.writeFileSync(path.join(projectRoot, ".vscode", "extensions.json"), `{
+fs.mkdirSync(path.join(projectRoot, '.vscode'));
+fs.writeFileSync(
+  path.join(projectRoot, '.vscode', 'extensions.json'),
+  `{
   "recommendations": ["svelte.svelte-vscode"]
 }
-`)
+`,
+);
 
-console.log("Converted to TypeScript.")
+console.log('Converted to TypeScript.');
 
-if (fs.existsSync(path.join(projectRoot, "node_modules"))) {
-  console.log("\nYou will need to re-run your dependency manager to get started.")
+if (fs.existsSync(path.join(projectRoot, 'node_modules'))) {
+  console.log('\nYou will need to re-run your dependency manager to get started.');
 }
