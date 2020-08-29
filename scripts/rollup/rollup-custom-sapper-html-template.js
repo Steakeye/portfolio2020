cda900baac4c1049fcce43e13c5f19042b6d4d96
Andrew Keats <andrew@steakeye.com>
2020-08-29 23:00:12 +0100

    wip: working on html template plugin

--- scripts/rollup/rollup-custom-sapper-html-template.js
+++ scripts/rollup/rollup-custom-sapper-html-template.js
@@ -1,18 +1,38 @@
+import fs from 'fs';
 import path from 'path';
+import getter from 'ramda/src/path'
 
 const defaultSourcePath = 'src/html.template';
 const defaultTargetPath = 'src/template.html';
 
+function transformTemplate(template, replacePairs, rollupConfig) {
+    replacePairs.forEach(({ templateKey, content, contentPath, contentTransformer } = {}) => {
+        let replacementContent = content || (contentPath ? getter(contentPath) || '': '');
+
+        if (contentTransformer) {
+            replacementContent = contentTransformer(replacementContent);
+        }
+
+        template = template.replace(RegExp(`\\$\\{${templateKey}}`, 'g'), replacementContent);
+    })
+
+    return template;
+}
+
 export default function (options = {}) {
     console.log('creating rollup-custom-sapper-html-template option')
     console.log('options', options)
-    const { source = defaultSourcePath, target = defaultTargetPath} = options
+    const { source = defaultSourcePath, target = defaultTargetPath, replacePairs = [],  } = options
+
+    let resolvedSourcePath;
 
     return {
         name: 'rollup-plugin-custom-sapper-html-template',
         buildStart(options) {
             console.info(this.name)
-            this.addWatchFile(path.resolve(source));
+            console.log('path.resolve(source)', path.resolve(source))
+            resolvedSourcePath = path.resolve(source);
+            this.addWatchFile(resolvedSourcePath);
         },
         /*load(...loadArgs) { //Gets called for every asset in the bundle
             console.log('loadArgs', loadArgs)
@@ -21,15 +41,20 @@ export default function (options = {}) {
             /* ... */
             console.log('resolveIdArgs', resolveIdArgs)
         },
-        generateBundle(...generateBundleArgs) {
+        generateBundle(rollupConfig) {
             /* ... */
-            console.log('generateBundleArgs', generateBundleArgs)
+            console.log('generateBundleArgs'); //, generateBundleArgs)
+            const htmlTemplate = fs.readFileSync(resolvedSourcePath, { encoding: 'utf8'});
+
+            const updatedHTMLTemplate = transformTemplate(htmlTemplate, replacePairs, rollupConfig);
+
+            fs.readFileSync(path.resolve(source), { encoding: 'utf8'});
         },
-        buildEnd(error) {
+        /*buildEnd(error) {
             if (!error) {
                 //this.addWatchFile(path.resolve(source));
             }
-        },
+        },*/
         // ...
     }
 }
