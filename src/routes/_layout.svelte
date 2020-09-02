24596a971e8f86dc17a56f90af1d879ab660117d
Andrew Keats <andrew@steakeye.com>
2020-09-02 17:44:49 +0100

    wip: experimenting with email redirects

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,15 +2,12 @@
   import styles from "./_layout.scss";
 
   import Nav from '../components/Nav.svelte'
-
-  //export let segment: string
 </script>
 
 <svelte:head>
   <title>Steakeye - Front End Development Services</title>
 </svelte:head>
 
-<!--Nav {segment} /-->
 <Nav />
 
 <main class={styles.content}>
---
+++ src/routes/email.ts
@@ -0,0 +1,16 @@
+import type { Response } from 'express';
+
+export async function get(req, res: Response, next) {
+
+    if (true) {
+        /*res.setHeader('Content-Type', 'application/json');*/
+        //res.end('email!'); //JSON.stringify(article));
+        //res.redirect('mailto:andrew@steakeye.com'); // No redirect built-in to polka!
+        res.writeHead(302, { Location : 'mailto:andrew@steakeye.com' });
+        //res.writeHead(302, { Location : '//google.com' });
+        res.end();
+        console.log('email redirect')
+    } else {
+        next();
+    }
+}
