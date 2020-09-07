b973591d29badedfee3ccbcd893367cc96709b11
Andrew Keats <andrew@steakeye.com>
2020-09-07 21:02:50 +0100

    wip: worked out how to import the svelte components after mounting

--- src/routes/_index/Breakout.svelte
+++ src/routes/_index/Breakout.svelte
@@ -1,6 +1,8 @@
 <script>
-    import {onMount} from 'svelte';
+    import { SvelteComponent } from 'svelte';
+    import { onMount } from 'svelte';
     import fragment from 'svelte-fragment';
+    //import Title from './breakout/Title.svelte';
 
     import roundelPath from '/src/assets/images/game/steakeye-roundel.svg';
 
@@ -15,43 +17,31 @@
     let Scene;
     let Text;
     let LoadingBar;
-    let LoadingBar;
+    let Arena;
+    let Title: SvelteComponent;
 
     onMount(async () => {
-        const sveltePhaser = await import('svelte-phaser');
+        /*const sveltePhaser = await import('svelte-phaser');
 
         Phaser = await import('phaser');
         Game = sveltePhaser.Game;
         Scene = sveltePhaser.Scene;
-        Text = sveltePhaser.Text;
+        Text = sveltePhaser.Text;*/
 
-        LoadingBar = await import('./breakout/LoadingBar.svelte')
+        /*LoadingBar = await import('./breakout/LoadingBar.svelte');
+        Arena = await import('./breakout/Arena.svelte');*/
+        Title = (await import('./breakout/Title.svelte')).default;
 
         beforeMount = false;
     });
 
-    //let progress;
-
 </script>
+
+<!--Title /-->
 
 {#if beforeMount}
     <div>Loading...</div>
 {:else}
-    <Game
-        width={400}
-        height={400}
-        physics={{ default: 'arcade' }}
-        scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}
-        noop-transparent="true"
-    >
-        <Scene
-            key="main"
-            preload={loadAssets}
-        >
-            <template use:fragment slot="loading" let:progress>
-                <svelte:component
-                        this={LoadingBar} x={400} y={400} />
-            </template>
-        </Scene>
-    </Game>
+    <svelte:component
+            this={Title} />
 {/if}
