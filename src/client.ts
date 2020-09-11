34e50f01d64ce03b748f37af275c0caa4e1322b0
Andrew Keats <andrew@steakeye.com>
2020-09-11 11:47:58 +0100

    adding class on body to help style content when js not enabled

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,8 @@
 import * as sapper from '@sapper/app'
 import './styles/global.scss'
 
+document.body.classList.add('js-enabled');
+
 sapper.start({
   target: document.querySelector('#sapper'),
 })
