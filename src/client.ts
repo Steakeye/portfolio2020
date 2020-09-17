f79fe953e252361de543710214e258adcdbdabaf
Andrew Keats <andrew@steakeye.com>
2020-09-17 14:35:38 +0100

    wip: work main headers including subtitle

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import * as sapper from '@sapper/app'
 import './styles/global.scss'
 
-document.body.classList.add('js-enabled');
+//document.body.classList.add('js-enabled');
 
 sapper.start({
   target: document.querySelector('#sapper'),
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
 <style lang="scss">
+  @use '../styles/colour.scss';
+
   %renderOver {
     position: relative;
     z-index: 5;
@@ -19,28 +21,33 @@
     font-family: Bungee Shade;
     color-scheme: blue;
     font-weight: 500;
-    color: #E8BF87;
+    color: colour.$brand-yellow;
     font-size: 4rem;
     margin-top: -4rem;
   }
 
-  p {
-    font-size: 1.6rem;
-    margin: 1em auto;
+  h2 {
+    color: colour.$brand-white;
+    font-size: 2.5rem;
+    font-family: Pacifico;
+    margin: 1em -2rem;
     position: fixed;
+    text-align: center;
     top: 50vh;
-    margin-left: -2rem;
-    padding: 0 25vw;
+    padding: 0 4rem;
     width: 100vw;
   }
 </style>
 
 <script>
+  import { pages } from '../resources/content.json';
   import Breakout from "./_index/Breakout.svelte";
   import styles from "./index.scss";
+
+  const { index: { header: { title, subTitle } } } = pages
 </script>
 
-<h1>Steakeye</h1>
-<p><strong>Senior Front-end Development Services</strong></p>
+<h1>{title}</h1>
+<h2>{subTitle}</h2>
 
 <Breakout className="{styles.breakoutWrapper}"/>
