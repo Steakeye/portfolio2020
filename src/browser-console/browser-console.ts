bc4e21c182a0bbea50f20c5d52a59e352725801d
Andrew Keats <andrew@steakeye.com>
2020-09-19 19:52:23 +0100

    wip: starting with the console interface

--- src/browser-console/browser-console.ts
+++ src/browser-console/browser-console.ts
@@ -1,2 +1,4 @@
 Object.defineProperty(window, 'help', { get: () => console.log('Welcome to Steakeye Browser Console') })
 Object.defineProperty(window, 'cv', { get: () => console.log('http://google.com') })
+
+console.log('TODO: Steakeye console welcome message')
