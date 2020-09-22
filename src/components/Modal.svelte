09530426eddfd04aed7b7f961bfc2d321a34d264
Andrew Keats <andrew@steakeye.com>
2020-09-22 03:11:16 +0100

    wip: work on Modal component

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -52,36 +52,85 @@
 </style>
 <script context="module">
     import { onMount } from 'svelte';
+    import {sleep} from "../utils/Runtime.ts";
     import { components } from '../resources/content.json';
 
     const { modal: { closeButton: { text: closeText } } } = components;
+
+    const storeKeyPrefixModal = 'modal#';
+
+    function assignModalBackgroundElProps(props, open:boolean, id?:string) {
+        if (id) {
+            props.id = id;
+        }
+
+        //if (open !== undefined) {
+        let hidden = !open;
+        props.hidden = hidden;
+        props['aria-hidden'] = hidden;
+        //}
+    }
 </script>
 <script>
     export let id = ''
+    export let targetId = ''
     export let className = ''
+    export let renderBeforeOpen = true;
+
+    const modalBGProps = {};
     let mounted = false;
     let open = true; //TODO change after debug
+    let wrapperEl: HTMLDivElement;
 
     function closeModal() {
         console.log('closeModal');
         open = false;
     }
 
-    onMount(() => {
+    assignModalBackgroundElProps(modalBGProps, id);
+
+    onMount(async () => {
         mounted = true;
+        setTimeout(() => console.log('wrapperEl', wrapperEl), 0);
+
+        //TODO: add open and close modal callbacks to sore with unique key
+        let modalId = targetId || id;
+
+        if (!modalId) {
+            console.warn(`Modal is missing 'targetId' or 'id' property set, looking for nearest child with 'id' attribute`);
+
+            await sleep(0);
+
+            //try to find first child element with an id to use as the target value
+            if (wrapperEl) {
+                const childWithId = Array.from(wrapperEl.children).filter((el: HTMLElement) => !!el.id)[0];
+
+                modalId = childWithId?.id;
+            }
+
+            if (!modalId) {
+                throw new Error(`Modal is missing 'targetId' or 'id', cannot create store entry for trigger bindings`)
+            }
+        }
+
+        const modalKey = `${storeKeyPrefixModal}${targetId || id}`;
+
+        return () => {
+            //TODO: teardown
+        }
     });
 </script>
 
-{#if !mounted}
+{#if !mounted && renderBeforeOpen}
     <slot />
-{:else}
+{:else if (mounted && renderBeforeOpen) || open}
     <div
-            id:id
+            {...modalBGProps}
             class="modal-background"
             class:open
             class:className
     >
-        <div class="modal-content-wrapper">
+        <div bind:this={wrapperEl} class="modal-content-wrapper">
             <slot />
             <button
                     class="close-button"
