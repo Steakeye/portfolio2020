6934489c486f33933113c1d674515103d245fa0b
Andrew Keats <andrew@steakeye.com>
2020-09-26 01:23:51 +0100

    fixing bug importing json into scss when string contains a comma...
    Solution is to allow for a string to contain a nested string and then have a util function to unquote the string and convert it to a JSON ('"' double quote) string for parsing, then parsing to unquote the string.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,13 +3,29 @@
   import { ui } from '../resources/config.json'
   import content from '../resources/content.json'
 
+  function unquoteMediaQueries(mediaQueryMap: { [key: string]: string}) {
+    const nestedStringTest = /^('|")(.+)\1$/;
+    const updatedMap = {}
+
+    for (let key in mediaQueryMap) {
+      const query = mediaQueryMap[key];
+
+      updatedMap[key] = nestedStringTest.test(query) ? JSON.parse(query.replace(nestedStringTest, `"$2"`)): query;
+    }
+
+    return updatedMap
+  }
+
   const { layout: { mediaQueries } } = ui;
   const{ title, metaDescription } = content.global;
   const copyRightMessage = content.global.partials.footer.copyright;
   const year = new Date().getFullYear();
+  const unquotedMediaQueries = unquoteMediaQueries(mediaQueries);
 </script>
 <script>
   import Nav from '../partials/Nav.svelte'
+
+  console.log('layout unquotedMediaQueries', unquotedMediaQueries);
 </script>
 
 <style>
@@ -46,7 +62,7 @@
   <meta name="description" content={metaDescription}>
 </svelte:head>
 
-<MediaQuery {mediaQueries}>
+<MediaQuery mediaQueries={unquotedMediaQueries}>
 <header>
 <Nav />
 </header>
