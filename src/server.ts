2debd9b7a8d4f93d47c7a013c63d15ad35f77a49
Andrew Keats <andrew@steakeye.com>
2020-10-05 12:51:49 +0100

    wip: adding extra CSP details to allow resources to load

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,59 @@
 import sirv from 'sirv';
 import polka from 'polka';
+import { v4 as uuidV4 } from 'uuid';
+import type { ServerResponse } from 'http';
 import compression from 'compression';
+import helmet from 'helmet';
 import * as sapper from '@sapper/server';
 import './styles/global.scss';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const helmetDirectivesDefault = {
+    "default-src": ["'self'"],
+    "base-uri": ["'self'"],
+    "block-all-mixed-content": [],
+    "font-src": ["'self'", "https:", "data:"],
+    "frame-ancestors": ["'self'"],
+    "img-src": ["'self'", "data:"],
+    "object-src": ["'none'"],
+    "script-src": ["'self'"],
+    "script-src-attr": ["'none'"],
+    "style-src": ["'self'", "https:", "'unsafe-inline'"],
+    "upgrade-insecure-requests": [],
+}
+
+const { ['img-src']: oldImageSrc, ['script-src']: oldScriptSrc, ...serverHelmetDirectives } = helmetDirectivesDefault;
+
+function numberOnce(req, res, next) {
+    console.log('res', res)
+    res.locals = res.locals ?? {};
+  res.locals.nonce = uuidV4();
+  next();
+}
+
 polka() // You can also use Express
-  .use(compression({ threshold: 0 }), sirv('public', { dev }), sapper.middleware())
+  .use(
+      numberOnce,
+      helmet({
+        contentSecurityPolicy: {
+          directives: {
+            ...serverHelmetDirectives,
+              imgSrc: [...oldImageSrc, 'blob:'],
+              connectSrc: dev ? [`'self'`, 'localhost:10000']: null,
+            scriptSrc: [
+                ...oldScriptSrc,
+              (req, res) => `'nonce-${(res as ServerResponse & { locals }).locals.nonce}'`,
+              'blob:',
+            ]
+          }
+        }
+      }),
+      compression({ threshold: 0 }),
+      sirv('public', { dev }),
+      sapper.middleware()
+  )
   .listen(PORT, (err) => {
     if (err) console.log('error', err);
   });
