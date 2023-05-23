b68f6b445a28cbbac7e5e254c1fe1b0c7230912f
Andrew Keats <andrew.keats@git.live>
2023-05-23 16:00:01 +0100

    wip: trying to get icon fonts to work in app

--- basic-update/vite.config.ts
+++ basic-update/vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'node_modules/@coreui/icons/fonts',
+					dest: 'public/assets'
+					//dest: '/'
+				},
+			]
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
