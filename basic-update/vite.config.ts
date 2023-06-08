0a8bfa4448d71f55453c66af3f201f3a0a858560
Andrew Keats <andrew.keats@git.live>
2023-06-08 18:14:17 +0100

    wip: working on inline css...

--- basic-update/vite.config.ts
+++ basic-update/vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import inlineCssModules from 'vite-plugin-inline-css-modules'
 import jsonImporter from 'node-sass-json-importer';
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
+	inlineCssModules()
+	sveltekit(),
 	],
 	css: {
 		preprocessorOptions: {
